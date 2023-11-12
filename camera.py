f42c2c11e727d23600a4d08f7f521675f2e7fa12
Connor Brown <browncw@lafayette.edu>
2023-11-12 07:42:13 -0500

    coord tweaks

--- camera.py
+++ camera.py
@@ -10,8 +10,8 @@ def write_report(report):
     fd.write(report)
 
 
-cap1 = cv2.VideoCapture(0)
-cap2 = cv2.VideoCapture(2)
+cap1 = cv2.VideoCapture(2)
+cap2 = cv2.VideoCapture(0)
 
 right = False
 left = False
@@ -117,20 +117,20 @@ while cap1.isOpened() and cap2.isOpened():
     x = (math.tan(radians1)*w+h)/(math.tan(radians2+(math.pi/2))-math.tan(radians1))
     y = math.tan(radians1)*(x+w)
 
-    if x < -50:
-        x = -50
-    if x > 50:
-        x = 50
-    if y < -50:
-        y = -50
-    if y > 50:
-        y = 50
+    # if x < -50:
+    #     x = -50
+    # if x > 50:
+    #     x = 50
+    # if y < -50:
+    #     y = -50
+    # if y > 50:
+    #     y = 50
     
     x += 50
     y += 50
 
-    x = (x/100)*8191
-    y = (y/100)*8191
+    x = (x/100)*16383
+    y = (y/100)*16383
 
     x = math.floor((x + prevX)/4)
     y = math.floor((y + prevY)/4)
