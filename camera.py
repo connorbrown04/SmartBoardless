c5a6eeabe902d5cbeddf2abf5c5beeac6606a233
Connor Brown <browncw@lafayette.edu>
2023-11-12 08:31:12 -0500

    swap camera

--- camera.py
+++ camera.py
@@ -12,8 +12,8 @@ def write_report(report):
     fd.write(report)
 
 
-cap1 = cv2.VideoCapture(2)
-cap2 = cv2.VideoCapture(0)
+cap1 = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(2)
 
 right = False
 left = False
